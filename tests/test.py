1ceeab0bc8421c477bd1bd09d1eaac73ee9fe6d8
JFermin <jfermin@iquartic.com>
2020-11-06 05:17:11 -0500

    create test cases

--- tests/test.py
+++ tests/test.py
@@ -7,3 +7,27 @@ class TestSavePrincessPeach(unittest.TestCase):
     def test_init_grid_file(self):
         with self.assertRaises(SystemExit):
             enter_castle('/Users/JFermin/Documents/GitHub/itsMeMario/tests/test_grids/init_grid.txt')
+
+    def test_init_grid_reversed(self):
+        with self.assertRaises(SystemExit):
+            enter_castle(
+                '/Users/JFermin/Documents/GitHub/itsMeMario/tests/test_grids/init_grid_reversed.txt'
+            )
+
+    def test_init_grid_flipped(self):
+        with self.assertRaises(SystemExit):
+            enter_castle(
+                '/Users/JFermin/Documents/GitHub/itsMeMario/tests/test_grids/init_grid_flipped.txt'
+            )
+
+    def test_init_grid_flipped_reversed(self):
+        with self.assertRaises(SystemExit):
+            enter_castle(
+                '/Users/JFermin/Documents/GitHub/itsMeMario/tests/test_grids/test_test_init_grid_flipped_reversed.txt'
+            )
+
+    def test_update_me(self):
+        with self.assertRaises(SystemExit):
+            enter_castle(
+                '/Users/JFermin/Documents/GitHub/itsMeMario/tests/test_grids/update_me.txt'
+            )
